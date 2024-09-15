dc099a2fa739309d3ccc561726413855b03bbae4
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-09-15 20:25:47 +0200

    Remove unneeded comment

--- apps/web/app/declarations/remix.ts
+++ apps/web/app/declarations/remix.ts
@@ -1,6 +1,6 @@
 declare module "@remix-run/server-runtime" {
 	// eslint-disable-next-line @typescript-eslint/consistent-type-definitions -- augmenting global interface
 	interface Future {
-		unstable_singleFetch: true; // 👈 enable _types_ for single-fetch
+		unstable_singleFetch: true;
 	}
 }
