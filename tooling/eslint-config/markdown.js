242b5f295f4b76a8fba1af1355b7444daafe2ed2
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-10-06 20:59:32 +0200

    Add markdown ESLint config

---
+++ tooling/eslint-config/markdown.js
@@ -0,0 +1,18 @@
+import tseslint from "typescript-eslint";
+import markdown from "@eslint/markdown";
+
+export default tseslint.config({
+	files: ["**/*.md"],
+	plugins: {
+		markdown,
+	},
+	language: "markdown/commonmark",
+	rules: {
+		"markdown/fenced-code-language": "error",
+		"markdown/heading-increment": "error",
+		"markdown/no-empty-links": "error",
+		"markdown/no-html": "error",
+		"markdown/no-invalid-label-refs": "error",
+		"markdown/no-missing-label-refs": "error",
+	},
+});
