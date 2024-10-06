84315d6ae9cc5c83f12c4a8eb8afaf588410c30b
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-10-06 20:23:07 +0200

    Adjust ESLint config

--- apps/web/app/declarations/remix.ts
+++ apps/web/app/declarations/remix.ts
@@ -1,5 +1,4 @@
 declare module "@remix-run/node" {
-	// eslint-disable-next-line @typescript-eslint/consistent-type-definitions -- augmenting global interface
 	interface Future {
 		unstable_singleFetch: true;
 	}
