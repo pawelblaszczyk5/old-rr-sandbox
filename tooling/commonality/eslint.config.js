2ecd180ba85fa4e5b9e386e03aeb07d9b7e2f477
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-10-15 20:37:28 +0200

    Add some basic custom check for commonality

---
+++ tooling/commonality/eslint.config.js
@@ -0,0 +1,21 @@
+import core from "@moneytor/eslint-config/core";
+import node from "@moneytor/eslint-config/node";
+
+export default [
+	{
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+	...core,
+	...node,
+	{
+		files: ["src/has-description-field.ts"],
+		rules: {
+			"import-x/no-default-export": "off",
+		},
+	},
+];
